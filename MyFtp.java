c71781aa65a7c94258fd4e9194182f3320997c8d
shubhis2 <75824164+shubhis2@users.noreply.github.com>
2021-01-25 03:56:13 -0500

    Update MyFtp.java

--- MyFtp.java
+++ MyFtp.java
@@ -5,54 +5,162 @@
 *****************************************************/
 import java.net.*;
 import java.io.*;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
+import java.util.*;
+
 
 public class MyFtp {
-  public static void main(String[] args) {
-
+  public static void main(String args[]) throws Exception {
     Socket client = null;
-    String host = args[0];
-    int port = Integer.parseInt(args[1]);
+    //String host = args[0];
+    int port = Integer.parseInt(args[0]);
     try {
-      client = new Socket(host, port);
+      client=new Socket("localhost",port);
+      MyFtp ftpc=new MyFtp();
 
-      // Set up I/O streams for server communication
-      DataInputStream input = new DataInputStream(client.getInputStream());
-      DataOutputStream output = new DataOutputStream(client.getOutputStream());
-
-      // keyboard entry
-      Scanner userEntry = new Scanner(System.in);
-      // read prompt from server
-      System.out.println("after Scanner: " + input.readUTF());
-
-      String commandIn;
-      int i;
-      do {
-        System.out.print("\nClient reads commands (int) to send to server:\n");
-        //commandIn = userEntry.nextLine();
-        i = userEntry.nextInt();
-        //send command to server
-  //      output.writeUTF(commandIn);
-        output.writeInt(i);
-
-        System.out.println("Back from server: " + input.readUTF());
-//      } while (commandIn != null);
-      } while (i != 0);
-    } catch (IOException e) {
+      while(true) { 
+        DataInputStream input=new DataInputStream(client.getInputStream());
+        DataOutputStream output=new DataOutputStream(client.getOutputStream());
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));   
+        String commandIn;
+        commandIn=br.readLine();
+        if(commandIn.equals("get"))
+        {
+          output.writeUTF("GET");
+          ftpc.getRemoteFile(client);
+        }
+        else if(commandIn.equals("put"))
+        {
+          output.writeUTF("PUT");
+          ftpc.putLocalFile(client);
+        }
+        else
+        {
+          output.writeUTF("quit");
+          System.exit(1);
+        }
+      }
+    }
+    catch (IOException e) 
+    {
       e.printStackTrace();
     }
-    catch (NoSuchElementException ne) {
+    catch (NoSuchElementException ne) 
+    {
       System.out.println("Connection closed");
     }
-    finally {
-      try {
+    finally 
+    {
+      try
+      {
         System.out.println("\n* Closing connection *");
         client.close();
-      } catch (IOException ioEx) {
+      }
+      catch (IOException ioEx)
+      {
         System.out.println("Unable to disconnect");
         System.exit(1);
       }
+    }    
+  }
+
+  public void putLocalFile(Socket s) throws Exception
+  {        
+    DataInputStream cin = new DataInputStream(s.getInputStream());
+    DataOutputStream cout=new DataOutputStream(s.getOutputStream());
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    String filename;
+    System.out.print("Enter File Name :");
+    filename=br.readLine();
+
+    File f=new File(filename);
+    if(!f.exists())
+    {
+      System.out.println("File not Exists...");
+      cout.writeUTF("File not found");
+      return;
+    }
+
+    cout.writeUTF(filename);
+
+    String servReply=cin.readUTF();
+    if(servReply.equals("File exists already"))
+    {
+      String option;
+      System.out.println("File exists already");
+      System.out.println("Overwrite? y/n ");
+      option=br.readLine();            
+      if(option.equals("y"))    
+      {
+        cout.writeUTF("y");
+      }
+      else
+      {
+        cout.writeUTF("n");
+        return;
+      }
+    }
+
+    System.out.println("Sending File ...");
+    FileInputStream fin=new FileInputStream(f);
+    int ch;
+    do
+    {
+      ch=fin.read();
+      cout.writeUTF(String.valueOf(ch));
+    }
+    while(ch!=-1);
+    fin.close();
+    System.out.println(cin.readUTF());
+
+  }
+
+  public void getRemoteFile(Socket s) throws Exception
+  {
+    DataInputStream cin = new DataInputStream(s.getInputStream());
+    DataOutputStream cout=new DataOutputStream(s.getOutputStream());
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+    String fileName;
+    System.out.print("Enter File Name :");
+    fileName=br.readLine();
+    cout.writeUTF(fileName);
+    String servReply=cin.readUTF();
+
+    if(servReply.equals("File Not Found"))
+    {
+      System.out.println("File not found on Server ...");
+      return;
+    }
+    else if(servReply.equals("READY"))
+    {
+      System.out.println("Receiving File ...");
+      File f=new File(fileName);
+      if(f.exists())
+      {
+        String option;
+        System.out.println("File exists already");
+        System.out.println("Overwrite? y/n ");
+        option=br.readLine();            
+        if(option.equals("n"))    
+        {
+          cout.flush();
+          return;    
+        }                
+      }
+      FileOutputStream fout=new FileOutputStream(f);
+      int ch;
+      String temp;
+      do
+      {
+        temp=cin.readUTF();
+        ch=Integer.parseInt(temp);
+        if(ch!=-1)
+        {
+          fout.write(ch);                    
+        }
+      }while(ch!=-1);
+      fout.close();
+      System.out.println(cin.readUTF());
     }
   }
 }
