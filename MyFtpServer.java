1ba99fa967af0609848df40819318631bc5c259e
shubhis2 <75824164+shubhis2@users.noreply.github.com>
2021-01-25 03:58:01 -0500

    Update MyFtpServer.java

--- MyFtpServer.java
+++ MyFtpServer.java
@@ -8,65 +8,132 @@ import java.net.*;
 import java.io.*;
 import java.util.*;
 
-public class MyFtpServer extends Thread {
-
+public class MyFtpServer extends Thread
+{
   static ServerSocket serverSocket;
-
-  public MyFtpServer(int port) throws IOException {
-    // create server socket on port input on command line
-    serverSocket = new ServerSocket(port);
-    // raise exeption if block on input takes longer than 1k seconds (`16 minutes`)
-    serverSocket.setSoTimeout(1000000);
-  }
-
-  public static void main(String[] args) {
-    // port number supplied on command line ie: MyFtpServer 9999
+  public static void main(String args[]) throws Exception
+  {
     int port = Integer.parseInt(args[0]);
+    serverSocket=new ServerSocket(port);
 
-    try {
-      Thread t = new MyFtpServer(port);
-      t.start();
-    } catch (IOException e) {
-      e.printStackTrace();
+    System.out.println("Server socket running ...");
+    // raise exeption if block on input takes longer than 1k seconds (`16 minutes`)
+    // serverSocket.setSoTimeout(1000000);
+    System.out.println("Waiting for client on port " + serverSocket.getLocalPort());
+    // accept client socket connection
+    Socket s = serverSocket.accept();
+    System.out.println("FTP Client connected to " + s.getRemoteSocketAddress());
+
+    MyFtpServer ftps=new MyFtpServer();
+    DataInputStream input;
+
+    while(true)
+    {
+      try
+      {
+        input=new DataInputStream(s.getInputStream());
+        System.out.println("Waiting for Command ...");
+        
+        //read commands from client - get/put/quit
+        String commandIn=input.readUTF();
+        if(commandIn.equals("GET"))
+        {
+          System.out.println("GET Command Received ...");
+          ftps.sendFile(s);
+        }
+        else if(commandIn.equals("PUT"))
+        {
+          System.out.println("PUT Command Receiced ...");                
+          ftps.receiveFile(s);
+        }
+        else if(commandIn.equals("quit"))
+        {
+          System.out.println("Quit Command Received ...");
+          System.exit(1);
+        }
+      }
+      catch(IOException i)
+      {
+        System.out.println(i);
+      }
+      catch(Exception e){
+
+      }
     }
   }
 
-  public void run() {
-      System.out.println("Server socket running ...");
 
-      while (true) {
-        try {
-          System.out.println("Waiting for client on port " + serverSocket.getLocalPort());
-          // accept client socket connection
-          Socket socket = serverSocket.accept();
+  public void sendFile(Socket s) throws Exception
+  {          
+    DataInputStream cin=new DataInputStream(s.getInputStream());
+    DataOutputStream cout=new DataOutputStream(s.getOutputStream()); 
+    String filename=cin.readUTF();
+    File f=new File(filename);
+    if(!f.exists())
+    {
+      cout.writeUTF("File Not Found");
+      return;
+    }
+    else
+    {
+      cout.writeUTF("READY");
+      FileInputStream fin=new FileInputStream(f);
+      int ch;
+      do
+      {
+        ch=fin.read();
+        cout.writeUTF(String.valueOf(ch));
+      }
+      while(ch!=-1);    
+      fin.close();    
+      cout.writeUTF("File Receive Successfully");                            
+    }
+  }
 
-          System.out.println("Just connected to " + socket.getRemoteSocketAddress());
+  public void receiveFile(Socket s) throws Exception
+  {
+    DataInputStream cin=new DataInputStream(s.getInputStream());
+    DataOutputStream cout=new DataOutputStream(s.getOutputStream());
+    String filename=cin.readUTF();
+    if(filename.equals("File not found"))
+    {
+      return;
+    }
+    File f=new File(filename);
+    String option;
 
-          DataInputStream input = new DataInputStream(socket.getInputStream());
-          DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+    if(f.exists())
+    {
+      cout.writeUTF("File exists already");
+      option=cin.readUTF();
+    }
+    else
+    {
+      cout.writeUTF("sendFile");
+      option="y";
+    }
 
-          output.writeUTF("1 myftpserver>>  ");
-
-        //  String command = input.readUTF();
-          int j = input.readInt();
-          do {
-            //  output.writeUTF("2 myftpserver>> >> " + command);
-            output.writeUTF("2 myftpserver>> " + j);
-
-          //    command = input.readUTF();
-          j = input.readInt();
-        //  } while (command != null);
-      } while (j != 0);
-          output.writeUTF("The End");
-          socket.close();
-
-        } catch(SocketTimeoutException s) {
-          System.out.println("Socket timed out");
-          break;
-        } catch (IOException e) {
-          e.printStackTrace();
-          break;
+    //if file exists and we want to overwrite it
+    if(option.equals("y"))
+    {
+      FileOutputStream fout=new FileOutputStream(f);
+      int ch;
+      String temp;
+      do
+      {
+        temp=cin.readUTF();
+        ch=Integer.parseInt(temp);
+        if(ch!=-1)
+        {
+          fout.write(ch);                    
         }
-      } // end while
-  } // end run
+      }while(ch!=-1);
+      fout.close();
+      cout.writeUTF("File Sent Successfully");
+    }
+    else
+    {
+      return;
+    }
+  }
 }
